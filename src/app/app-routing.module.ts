cab4b7dfa74edf8238aa1b1c799632b32c561184
Adolfo Mendoza <adolfitocraftero1225@gmail.com>
2023-11-03 21:03:44 -0600

    03/11/2023 AAMP

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu/menu.component';
+import { DatosAlumnoComponent } from './formulario/datos-alumno/datos-alumno.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo:'/home', pathMatch:'full'},
+  {path : 'home', component:HomeComponent},
+  {path: 'Agregar', component: DatosAlumnoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenuComponent } from './menu/menu/menu.component';
+import { MaterialModule } from './material/material/material.module';
+import { DatosAlumnoComponent } from './formulario/datos-alumno/datos-alumno.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    DatosAlumnoComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
